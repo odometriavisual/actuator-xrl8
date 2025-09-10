24e2d3c961900c475d297fb5122598004f4ff5e9
Fernando <caetanof@alunos.utfpr.edu.br>
2025-09-10 13:49:56 -0300

    refac: move all commands and status getters to the app class

--- main.py
+++ main.py
@@ -12,15 +12,66 @@ except RuntimeError:
 from actuator_xrl8.gcode_interpreter import GcodeInterpreter
 
 
-def main():
-    app = Flask(
-        __name__, static_url_path="/assets", static_folder="./frontend/dist/assets"
-    )
-    ws = SocketIO(app, cors_allowed_origins="*")
+class ActuatorApp(Flask):
+    def __init__(self):
+        super().__init__(
+            __name__, static_url_path="/assets", static_folder="./frontend/dist/assets"
+        )
 
-    app.machine = GcodeMachine()
-    app.interpreter = None
-    app.running = False
+        self.machine = GcodeMachine()
+        self.interpreter = None
+        self.running = False
+        self.last_gcode = None
+
+    def is_running(self):
+        return self.running
+
+    def is_calibrated(self):
+        return self.machine.is_calibrated()
+
+    def is_trajectory_initialized(self):
+        return self.interpreter is not None
+
+    def initialize_trajectory(self, gcode_src):
+        self.running = True
+        self.interpreter = GcodeInterpreter(gcode_src, self.machine)
+        self.interpreter.step()
+        self.running = False
+
+    def reinitialize_last_trajectory(self):
+        self.initialize_trajectory(self.last_gcode)
+
+    def step(self):
+        self.running = True
+        if status := self.interpreter.step():
+            if status is not True:
+                print(f"{status}")
+
+        if self.interpreter.is_finished():
+            self.interpreter = None
+        self.running = False
+
+    def play(self):
+        if self.interpreter is not None:
+            self.running = True
+
+            while status := self.interpreter.step():
+                if status is not True:
+                    print(f"{status}")
+                    break
+
+            if self.interpreter.is_finished():
+                self.interpreter = None
+
+            self.running = False
+
+    def pause(self):
+        self.machine.pause()
+
+
+def main():
+    app = ActuatorApp()
+    ws = SocketIO(app, cors_allowed_origins="*")
 
     @app.route("/")
     def index():
@@ -28,48 +79,27 @@ def main():
 
     @ws.on("gcode")
     def gcode(gcode_src):
-        app.running = True
-        app.interpreter = GcodeInterpreter(gcode_src, app.machine)
-        app.interpreter.step()
-        app.running = False
+        app.initialize_trajectory(gcode_src)
 
     @ws.on("step")
     def step():
-        app.running = True
-        if status := app.interpreter.step():
-            if status is not True:
-                print(f"{status}")
-
-        if app.interpreter.is_finished():
-            app.interpreter = None
-        app.running = False
+        app.step()
 
     @ws.on("play")
     def play():
-        if app.interpreter is not None:
-            app.running = True
-
-            while status := app.interpreter.step():
-                if status is not True:
-                    print(f"{status}")
-                    break
-
-            if app.interpreter.is_finished():
-                app.interpreter = None
-
-            app.running = False
+        app.play()
 
     @ws.on("pause")
     def pause():
-        app.machine.pause()
+        app.pause()
 
     def send_status():
         while True:
             status = {
-                "running": app.running,
-                "gcode_loaded": app.interpreter is not None,
+                "running": app.is_running(),
+                "gcode_loaded": app.is_trajectory_initialized(),
                 "pos": app.machine.get_position(),
-                "calibrated": app.machine.is_calibrated(),
+                "calibrated": app.is_calibrated(),
             }
             ws.emit("status", status)
             sleep(1 / 30)
